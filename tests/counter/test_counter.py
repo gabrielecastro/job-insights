651e8f4cfbdc1bb243af90d4842ea90a9827de1e
Gabriele <gabrielerayane2001@gmail.com>
2022-08-08 22:29:57 -0300

    test: requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    assert count_ocurrences(path, "products") == 1394
+    assert count_ocurrences(path, "Products") == 1394
+    assert count_ocurrences(path, "python") == 1639
+    assert count_ocurrences(path, "Python") == 1639
